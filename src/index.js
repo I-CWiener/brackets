90fde82e53bcde85de799322c725bf1b4ac2099b
I-CWiener <150772092+I-CWiener@users.noreply.github.com>
2024-09-16 10:33:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = [];
+
+  const brackets = new Map(bracketsConfig);
+console.log(brackets);
+
+  for (const bracket of str) {
+      console.log(bracket);
+    if (bracket === brackets.get(arr[arr.length - 1])) {
+      arr.pop();
+      console.log(arr)
+    } else {
+      arr.push(bracket);
+       console.log(arr);
+    }
+  }
+console.log(arr.length === 0);
+  return arr.length === 0;
 }
